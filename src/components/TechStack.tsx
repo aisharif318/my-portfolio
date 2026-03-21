19f568a5db5b7873d9efe04d50e86d9f4a54cc8a
Israfil Sharif <israfilsharif58@gmail.com>
2026-03-21 22:49:50 +0600

    Update tech stack images

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -13,14 +13,10 @@ import {
 
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
-  "/images/react2.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
-  "/images/typescript.webp",
-  "/images/javascript.webp",
+  "/images/pngwing.com.png",
+  "/images/pngwing.com (1).png",
+  "/images/pngwing.com (2).png",
+  "/images/pngwing.com (3).png",
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
